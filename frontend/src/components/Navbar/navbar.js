bbf7484aaf8035a014591217374cceb1082c331f
Adelshkeir <adelshkeir12@gmail.com>
2023-11-03 09:54:49 +0200

    finished navbar

--- frontend/src/components/Navbar/navbar.js
+++ frontend/src/components/Navbar/navbar.js
@@ -5,24 +5,25 @@ const Navbar = () => {
     <header>
       <div className="Navbar">
    
-   
+   <Link to ="/">
           <img src="https://cdn.discordapp.com/attachments/1159745951066497035/1168922949055688795/ShopEZSave__2_-removebg-preview.png?ex=65538754&is=65411254&hm=2e3dce2e367bc01eae562020bceec519f44b079c5712fad4d6ae1d4f06de987d&"></img>
+     </Link>
      
-     
-     
-          <Link to="/">
+     <div className="secondnav">
+          <Link to="/" className="link">
             <h1>Home</h1>
         </Link>
 
         
-        <Link to ="/Offers">
+        <Link to ="/Offers" className="link">
           <h1>Offers</h1>
         </Link>
 
 
-        <Link to = "/About">
+        <Link to = "/About" className="link">
           <h1>About</h1>
         </Link>
+        </div>
       </div>
     </header>
   );
